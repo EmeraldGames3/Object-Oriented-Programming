d6ba39eb98389beca1cddb18e48ac6fb4b264ff2
Bogdan Oprisiu <bobo.oprisiu@gmail.com>
2023-05-20 14:20:31 +0300

    Made ID mandatory for all objects

---
+++ Seminar/Seminar5/cmake-build-debug/CMakeFiles/Seminar5.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Seminar5.dir/main.cpp.obj"
+  "CMakeFiles/Seminar5.dir/main.cpp.obj.d"
+  "Seminar5.exe"
+  "Seminar5.exe.manifest"
+  "Seminar5.pdb"
+  "libSeminar5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Seminar5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
